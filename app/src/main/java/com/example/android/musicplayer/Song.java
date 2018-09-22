78a87ce0bfc4ae58b43ff7c964520e013e938603
Lolamocha <manola.quinti@gmail.com>
2018-09-22 12:40:15 +0200

    changes

--- app/src/main/java/com/example/android/musicplayer/Song.java
+++ app/src/main/java/com/example/android/musicplayer/Song.java
@@ -7,7 +7,7 @@ public class Song {
     private String ArtistName;
     private String AlbumName;
     private int Thumbnail;
-    private static int Sonoro;
+    private  int Sonoro;
 
     public Song(String titleSong, String artistName, String albumName, int thumbnail,int sonoro) {
         TitleSong = titleSong;
@@ -21,5 +21,5 @@ public class Song {
     String getArtistName() {return ArtistName;}
     String getAlbumName() {return AlbumName;}
     int getThumbnail() {return Thumbnail;}
-    static int getSonoro(){return Sonoro;}
+     int getSonoro(){return Sonoro;}
 }
